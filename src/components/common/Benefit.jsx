11bb924204c5f0e10c413bb143eb680d962a8f6c
Augustin <rjja@outlook.fr>
2024-12-07 16:15:44 +0300

    test7

--- src/components/common/Benefit.jsx
+++ src/components/common/Benefit.jsx
@@ -1,94 +1,76 @@
+import {Card} from "@/components/ui/card";
+
+const benefice = [{
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+}, {
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+}, {
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+}, {
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+}, {
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+}, {
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+}, {
+    id: 1,
+    title: 'Flexible Learning Schedule',
+    description: 'Fit your coursework around your existing commitments and obligations.',
+},]
 
 
-export default function Benefits () {
-    return (
-        <main  className="   mx-auto">
-{/*text Benefits*/}
-                <section className=" container  mx-auto flex justify-between mt-10 " >
-                    <div className="">
-                        <h1 className="font-bold text-3xl ">Benefits</h1>
-                        <p className="mt-5">Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. <br></br>Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.</p>  
+export default function Benefits() {
+    return (<section className="container mx-auto">
+        <div className="">
+            <div className="">
+                <h1 className="font-bold text-3xl ">Benefits</h1>
+                <p className="mt-5">Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id
+                    imperdiet et. <br></br>Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit
+                    fringilla feugiat senectus in.</p>
+            </div>
+            <button className="">View All</button>
+        </div>
+        <div className="">
+            <div
+                // grid 3 cards
+                className={"grid grid-cols-3 gap-5 mt-5"}
+            >
+                {benefice.map((element, index) => <Card  key={index}
+                    className={"shadow-none rounded-none"}
+                >
+                    <p className="font-bolds text-6xl text-right">{index +1}</p>
+                    <h3 className="font-bold text-xl ">
+                        {element.title}
+                    </h3>
+                    <p className="flex">
+                        {element.description}
+                    </p>
+                    <div>
+                        <svg className="" width="74" height="65" viewBox="0 0 74 74"
+                             fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD"/>
+                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                  d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z"
+                                  fill="#FF9500"/>
+                        </svg>
                     </div>
-                    <button className= "border h-10 w-20 mt-16 mr-32">View All</button>
-                </section> 
-{/*colun 1-3*/}
-                <section className=" flex   flex-col  mt-10 ">
-                    <section className="flex container mx-auto gap-5 ">
+                </Card>)}
 
-                      <div className="flex  border w-1/3  h-80 flex-col p-7 ">
-                        <p className="font-bolds text-6xl text-right">01</p>
-                        <h3 className="font-bold text-xl ">Flexible Learning Schedule</h3>
-                        <p className="flex">Fit your coursework around your existing commitments and obligations.</p>
-
-                        <svg className="flex self-end    mt-9 " width="74" height="65" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD" />
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3" />
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z" fill="#FF9500" />
-                        </svg>
-
-                     </div>
-                     <div className="flex  border w-1/3  h-80 flex-col p-7 ">
-                        <p className="font-bolds text-6xl text-right">02</p>
-                        <h3 className="font-bold text-xl ">Expert Instruction</h3>
-                        <p>Learn from industry experts who have hands-on experience in design and development.</p>
-                        <svg className="flex self-end    mt-9 " width="74" height="65" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD" />
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3" />
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z" fill="#FF9500" />
-                        </svg>
-
-
-                     </div>
-                     <div className="flex  border w-1/3 h-80 flex-col p-7 ">
-                        <p className="font-bolds text-6xl text-right">03</p>
-                        <h3 className="font-bold text-xl ">Diverse Course Offerings</h3>
-                        <p>Explore a wide range of design and development courses covering various topics.</p>
-                        <svg className="flex self-end    mt-9 " width="74" height="65" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD" />
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3" />
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z" fill="#FF9500" />
-                        </svg>
-
-                     </div>
-                    </section>
-                    {/*colun 3-6*/}
-                    <section className="flex  container mx-auto gap-5 mt-5">
-                     <div className="flex  border w-1/3  h-80 flex-col p-7 ">
-                        <p className="font-bolds text-6xl text-right">04</p>
-                        <h3 className="font-bold text-xl ">Updated Curriculum</h3>
-                        <p>Access courses with up-to-date content reflecting the latest trends and industry practices.</p>
-                        <svg className="flex self-end mt-9 " width="74" height="65" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD" />
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3" />
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z" fill="#FF9500" />
-                        </svg>
-
-
-                     </div>
-                     <div className="flex  border w-1/3  h-80 flex-col p-7 ">
-                        <p className="font-bolds text-6xl text-right" >05</p>
-                        <h3 className="font-bold text-xl ">Practical Projects and Assignments</h3>
-                        <p>Develop a portfolio showcasing your skills and abilities to potential employers.</p>
-                        <svg className="flex self-end    mt-9 " width="74" height="65" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD" />
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3" />
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z" fill="#FF9500" />
-                        </svg>
-
-                     </div>
-                     <div className="flex  border w-1/3 h-80 flex-col p-7 ">
-                        <p className="font-bolds text-6xl text-right">06</p>
-                        <h3 className="font-bold text-xl ">Interactive Learning Environment</h3>
-                        <p>Collaborate with fellow learners, exchanging ideas and feedback to enhance your understanding.</p>
-                        <svg className="flex self-end    mt-9 " width="74" height="65" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" fill="#FCFCFD" />
-                            <rect x="0.5" y="0.5" width="73" height="73" rx="7.5" stroke="#F1F1F3" />
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M28.8734 45.1266C29.3714 45.6245 30.1786 45.6245 30.6766 45.1266L42.95 32.8531V42.525C42.95 43.2292 43.5208 43.8 44.225 43.8C44.9292 43.8 45.5 43.2292 45.5 42.525V29.775C45.5 29.0708 44.9292 28.5 44.225 28.5H31.475C30.7708 28.5 30.2 29.0708 30.2 29.775C30.2 30.4792 30.7708 31.05 31.475 31.05H41.1469L28.8734 43.3234C28.3755 43.8214 28.3755 44.6286 28.8734 45.1266Z" fill="#FF9500" />
-                        </svg>
-
-                     </div>
-                    </section>
-                </section>
-        </main>
-    );
+            </div>
+        </div>
+    </section>);
 }
--- src/components/common/Course.jsx
+++ src/components/common/Course.jsx
@@ -1,119 +1,95 @@
-import { Section } from "lucide-react";
+import {Section} from "lucide-react";
 
 import Image from "next/image";
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
 
 
-export default function Courses () {
-    return (
-        <main className="border">
-    
-            <Section >
-                <div>
-                    <h1>OursCourses</h1>
-                    <p>
-                    Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. 
+const courseList = [{
+    image: '/images/course/coursImage1.png',
+    duration: '4Weeks',
+    level: 'Beginner',
+    author: 'John Smith',
+    title: 'Web Design Fundamentals',
+    description: 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.'
+},
+
+    {
+        image: '/images/course/coursImage2.png',
+        duration: '4Weeks',
+        level: 'Beginner',
+        author: 'John Smith',
+        title: 'Web Design Fundamentals',
+        description: 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.'
+    }, {
+        image: '/images/course/coursImage3.png',
+        duration: '4Weeks',
+        level: 'Beginner',
+        author: 'John Smith',
+        title: 'Web Design Fundamentals',
+        description: 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.'
+    }, {
+        image: '/images/course/coursImage4.png',
+        duration: '4Weeks',
+        level: 'Beginner',
+        author: 'John Smith',
+        title: 'Web Design Fundamentals',
+        description: 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.'
+    }, {
+        image: '/images/course/coursImage5.png',
+        duration: '4Weeks',
+        level: 'Beginner',
+        author: 'John Smith',
+        title: 'Web Design Fundamentals',
+        description: 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.'
+    }, {
+        image: '/images/course/coursImage6.png',
+        duration: '4Weeks',
+        level: 'Beginner',
+        author: 'John Smith',
+        title: 'Web Design Fundamentals',
+        description: 'Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques.'
+    },]
+
+export default function Courses() {
+    return (<div className="container mx-auto">
+        <div>
+            <div>
+                <h1>OursCourses</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras
+                    eu sit dignissim lorem nibh et.
                     Ac cum eget habitasse in velit fringilla feugiat senectus in.
-                    </p>
-                </div>
-                <button>View All</button>     
-            </Section>
+                </p>
+            </div>
+            <button>View All</button>
+        </div>
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))",
+                gap: "1rem",
+                padding: "1rem",
+            }}
+        >
+            {courseList.map((course, index) => {
+                return (<Card
+                    key={index}
+                >
+                    {/*<CardHeader>*/}
+                    {/*    <Image src={course.image} alt={course.title} width={400} height={200}/>*/}
+                    {/*    <h1>{course.title}</h1>*/}
+                    {/*    <p>{course.description}</p>*/}
+                    {/*</CardHeader>*/}
+                    <CardContent>
+                        <Image src={course.image} alt={course.title}
+                               width={400} height={200}/>
+                        <p>Duration: {course.duration}</p>
+                        <p>Level: {course.level}</p>
+                        <p>Author: {course.author}</p>
+                    </CardContent>
+                </Card>);
+            })}
+        </div>
 
-            <section>
-                
-                <div>
-                    <div>
-                    <button>4Weeks</button>
-                    <button>Beginner</button>
-                    </div>
-                    <p>By John Smith</p>
-                </div>
-                <div>
-                    <h>Web Design Fundamentals</h>
-                    <p>Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX).
-                         Learn design principles, wireframing, prototyping, and usability testing techniques.</p>
-                </div>
-                <button>Get it Now</button>
-            </section>
-
-            <section>
-                <div>
-                    <div>
-                    <button>6Weeks</button>
-                    <button>Intermediate</button>
-                    </div>
-                    <p>By Emily johson</p>
-                </div>
-                <div>
-                    <h>UI/UX Design</h>
-                    <p>Dive into the world of mobile app development. 
-                        Learn to build native iOS and Android applications using industry-leading frameworks like Swift and Kotlin.</p>
-                </div>
-                <button>Get it Now</button>
-            </section>
-
-            <section>
-                <div>
-                    <div>
-                    <button>8Weeks</button>
-                    <button>Intermediate</button>
-                    </div>
-                    <p>By David Brown</p>
-                </div>
-                <div>
-                    <h>Mobile App Developmrnt</h>
-                    <p>Discover the fundamentals of graphic design, including typography, color theory, layout design, and image manipulation techniques. 
-                        Create visually stunning designs for print and digital media.</p>
-                </div>
-                <button>Get it Now</button>
-            </section>
-
-            <section>
-                <div>
-                    <div>
-                    <button>10 Weeks</button>
-                    <button>Beginner</button>
-                    </div>
-                    <p>By Sarah Thompson</p>
-                </div>
-                <div>
-                    <h>Graphic Desing for Beginners</h>
-                    <p>Become proficient in front-end web development. Learn HTML, CSS, JavaScript, and popular frameworks like Bootstrap and React. 
-                        Build interactive and responsive websites.</p>
-                </div>
-                <button>Get it Now</button>
-            </section>
-
-            <section>
-                <div>
-                    <div>
-                    <button>10Weeks</button>
-                    <button>Intermediate</button>
-                    </div>
-                    <p>By Michael Adams</p>
-                </div>
-                <div>
-                    <h>Front-End Web Development</h>
-                    <p>Take your JavaScript skills to the next level. Explore advanced concepts like closures,
-                         prototypes, asynchronous programming, and ES6 features. Build complex applications with confidence.</p>
-                </div>
-                <button>Get it Now</button>
-            </section>
-
-            <section>
-                <div>
-                    <div>
-                    <button>4Weeks</button>
-                    <button>Advance</button>
-                    </div>
-                    <p>By Jennifer Wilson</p>
-                </div>
-                <div>
-                    <h>We</h>
-                    <p></p>
-                </div>
-                <button>Get it Now</button>
-            </section>
-
-        </main>
-    );
+    </div>);
 }
